1bb86ba0d9de8c142227c77fba084da935a94436
Bruno Pastre <pastre68@gmail.com>
2021-03-19 22:39:16 -0300

    Initial Commit

---
+++ LearningJourney/LearningJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningJourneyApp.swift
+//  LearningJourney
+//
+//  Created by Bruno Pastre on 19/03/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
